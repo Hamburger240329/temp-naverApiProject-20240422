c3d9f17babafdaf9a37a9cbfe007c8813985c688
gyojincompany <luckyred@gmail.com>
2024-04-26 11:44:19 +0900

    뉴스 검색 결과 출력 및 해당 신문사 링크 연결 기능 추가

--- naverNewsApp.py
+++ naverNewsApp.py
@@ -5,6 +5,8 @@ from PyQt5 import uic
 
 from naverSearchApi import *
 
+import webbrowser
+
 form_class = uic.loadUiType("ui/naverNewsSearchAppUi.ui")[0]  # 외부에서 ui 불러오기
 
 class MainWindow(QMainWindow, form_class):
@@ -17,6 +19,8 @@ class MainWindow(QMainWindow, form_class):
         self.statusBar().showMessage("Naver News Search Application v1.0")
 
         self.searchBtn.clicked.connect(self.searchBtn_clicked)
+        self.result_table.doubleClicked.connect(self.link_doubleClicked)
+        # 테이블의 항목이 더블클릭되면 link_doubleClicked 함수 호출
 
     def searchBtn_clicked(self):
         keyword = self.input_keyword.text()  # 사용자가 입력한 검색 키워드 가져오기
@@ -44,6 +48,25 @@ class MainWindow(QMainWindow, form_class):
         self.result_table.setColumnWidth(2, 120)
         # 테이블에 출력되는 검색결과 수정 금지 기능 추가
         self.result_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        for i, news in enumerate(newsResult):  # i-> 0~9
+            newsTitle = news['title']  # 뉴스 제목
+            newsTitle = newsTitle.replace('&quot','').replace(';','').replace('<b>','').replace('</b>','')
+            newsLink = news['originallink']  # 뉴스의 오리지널 url 링크
+            newsDate = news['pubDate']  # 뉴스 게시일
+            newsDate = newsDate[0:25]
+
+            self.result_table.setItem(i, 0, QTableWidgetItem(newsTitle))
+            self.result_table.setItem(i, 1, QTableWidgetItem(newsLink))
+            self.result_table.setItem(i, 2, QTableWidgetItem(newsDate))
+
+    def link_doubleClicked(self):  # 링크를 더블클릭하면 호출되는 함수
+        selectedRow = self.result_table.currentRow()  # 현재 더블클릭하여 선택되어 있는 행의 인덱스를 가져오기
+        selectedLink = self.result_table.item(selectedRow, 1).text()  # 더블클릭한 셀의 텍스트를 가져오기
+        webbrowser.open(selectedLink)
+
+
+
 
 
 
